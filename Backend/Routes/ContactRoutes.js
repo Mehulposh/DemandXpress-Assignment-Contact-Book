165c4d752b2f408e5ca776dd69871ca84956d2da
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-09-19 09:27:23 +0530

    added controller functions to the routes

--- Backend/Routes/ContactRoutes.js
+++ Backend/Routes/ContactRoutes.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const {getAllContacts,createContact,deleteContactByID} = require('../Controller/ContactController');
+
 const router = express.Router()
 
-router.post('/')
-router.get('/')
-router.delete('/:id')
+router.post('/',createContact)
+router.get('/',getAllContacts)
+router.delete('/:id',deleteContactByID)
 
 module.exports = router
