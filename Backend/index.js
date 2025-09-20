c29ffe5c7852bfb71e95ee024830fbf6982b153e
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-09-20 09:43:02 +0530

    changes in cors

--- Backend/index.js
+++ Backend/index.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT;
 const server = express();
 
 //handle cors for different source access
-server.use(cors());
+server.use(cors({
+  origin: ["https://demand-xpress-assignment-contact-bo.vercel.app"], // your frontend domain
+  methods: ["GET", "POST", "DELETE"],
+  credentials: true
+}));
 
 //universal middleware
 server.use(express.json());
