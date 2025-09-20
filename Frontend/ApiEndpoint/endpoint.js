989e765886171a88b7889c72eaa4ea77aba88b1e
Mehul Poshattiwar <mehulposhattiwar4995@gmail.com>
2025-09-20 09:47:03 +0530

    changes in backend endpoint

--- Frontend/ApiEndpoint/endpoint.js
+++ Frontend/ApiEndpoint/endpoint.js
@@ -1,5 +1,5 @@
 const backend = {
-    'endpoint': 'https://demandxpress-assignment-contact-book.onrender.com'
+    'endpoint': 'https://demandxpress-assignment-contact-book.onrender.com/api/v1/contacts'
 }
 
 export default backend;
